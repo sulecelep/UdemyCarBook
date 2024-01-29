668d959921056bda6b72567fe8c13b3d64b8053f
ssule <ssulecelepp@gmail.com>
2024-01-29 19:48:01 +0300

    7. Bölüm tamamlandı. About Consume.

---
+++ Frontends/UdemyCarBook.Dto/AboutDtos/ResultAboutDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Dto.AboutDtos
+{
+    public class ResultAboutDto
+    {
+        public int AboutID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+}
---
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponents
+{
+	public class _TestimonialComponentPartial:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7028/api/Testimonials");
+			if(responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData= await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<string>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
